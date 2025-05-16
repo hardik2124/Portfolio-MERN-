3249c89545a651882087c83ea791c2ce49206bd3
hardik <hardikkachhadiya212@gmail.com>
2025-05-16 16:29:56 +0530

    5154

--- server/server.js
+++ server/server.js
@@ -24,7 +24,7 @@ console.log('Using Cloudinary for image storage');
 // Middleware
 app.use(express.json());
 app.use(cors({
-  origin: ['http://localhost:5173', 'https://portfolio-mern-ioob.vercel.app/','http://localhost:5173'],
+  origin: ['http://localhost:5173', 'https://portfolio-mern-ioob.vercel.app/','http://localhost:5174'],
   credentials: true
 }));
 app.use(morgan('dev'));
